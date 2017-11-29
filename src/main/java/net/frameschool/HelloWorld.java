64b23db385101929e397471a43cf53c9cf352004
thunder <gianni.amendola@gmail.com>
2017-11-29 12:54:03 +0100

    first commit all

---
+++ src/main/java/net/frameschool/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.frameschool;
+
+/**
+ * Created by thunder on 29/11/17.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] arg) {
+
+        System.out.println("Hello World");
+    }
+}
